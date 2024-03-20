86fc84b36c13f0e84e0735461c534ba482f6dcc6
Nicklas Thor <nicke.tor.86@gmail.com>
2024-03-20 14:14:51 +0100

    Create Questions.js
    recreate Questions.js

---
+++ quiz/src/components/Questions.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Questions = () => {
+  return (
+    <div style={{ display: 'none' }}>
+      {/* Add your content here */}
+    </div>
+  );
+};
+
+export default Questions;
