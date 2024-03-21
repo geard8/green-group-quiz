989ec9aed5a4e3e36a2555361ae558aa9296f355
jerryH12 <jerry.hall2018@hotmail.com>
2024-03-21 09:36:42 +0100

    Text for start page.

--- quiz/src/App.js
+++ quiz/src/App.js
@@ -10,7 +10,19 @@ const App = () => {
     <div className="container" style={{flex:"1"}}>
       <article>
         <h1>Vad för husdjur passar dig bäst?</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>När man ska skaffa husdjur är det några saker man absolut behöver tänka på.</p>
+        <h2>Livsstil</h2>
+        <p>Vilken typ av livsstil har du? Ofta hemma eller bortrest? Aktiv på fritiden? Sådant är såklart viktiga faktorer som spelar in i valet av husdjur.</p>
+        <h2>Allergier</h2>
+        <p>
+        Detta är kanske mer självklart. Är man allergisk mot exempelvis katter ska man givetvis se sig om efter ett annat husdjur. Bör också fundera över om någon i ens omgivning är allergisk. 
+        </p>
+        
+        <h2>Ekonomiska aspekter</h2>
+       <p>
+       Det kan vara dyrt med husdjur. Inte bara mat utan även veterinärkostnader. Man vill ju att ens husdjur ska må bra. 
+       </p>
+        
       </article>
       <Questions />
     </div>
